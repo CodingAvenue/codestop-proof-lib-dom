fd68f1bfa7d7ae46d19a259e985837f76794af10
Jerome Suarez <jerome.suarez@codingavenue.com>
2019-03-20 23:15:21 +0800

    Fix index.js file incorrect code. Also updated js proof library version

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const Proof = require('./lib/proof');
-
-const html = '/home/tenshilyle/index.html';
-
-Proof.initialize(html)
-    .then((proof) => {
-        console.log(proof);
-        console.log(proof.hasJS())
-    })
-    .catch(e => console.log(e));
+module.exports = require('./lib/proof');
